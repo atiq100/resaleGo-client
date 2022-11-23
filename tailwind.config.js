e30574761fa43e3f89323d368c0b1493264d400d
atiq100 <atiqur1997rj@gmail.com>
2022-11-23 23:42:54 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#ea580c",
+        
+"secondary": "#4cd359",
+        
+"accent": "#f472b6",
+        
+"neutral": "#231D2B",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#2dd4bf",
+        
+"success": "#84cc16",
+        
+"warning": "#b91c1c",
+        
+"error": "#E55E57",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")]
 }
